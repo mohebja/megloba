061375a2ba8657c9eb998d0e79fbf857c2c77de3
mohebja <mohebja@gmail.com>
2026-09-12 20:41:27 +0330

    Add AutoBackupManager for scheduled backups

---
+++ security/src/main/java/com/global/sms/security/backup/AutoBackupManager.kt
@@ -0,0 +1,9 @@
+package com.global.sms.security.backup
+
+import android.content.Context
+
+object AutoBackupManager {
+    fun init(context: Context) {
+        // Scheduled auto-backup initialization
+    }
+}
